fdb78d577bf4f90ecd48dbf02f140d1e9c36d99e
uilacceb <qu6qu6g@gmail.com>
2025-02-28 16:41:51 -0500

    added header icon

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import styles from "./Header.module.css"
-import cartIcon from "../../assets/carts.png"
+import mainIcon from "../../assets/header-icon.png"
+import cartImage from "../../assets/carts.png"
 
 const Header = () => {
+  const url = "https://i0.wp.com/www.clubit.tv/wp-content/uploads/2017/08/pokemon.jpg"
   return (
     <div className={styles.container}>
-      <img className={styles.cartImage} src={cartIcon} height={40} width={40} />
+      <img src={mainIcon} height={80} />
+      <img className={styles.cartImage} src={cartImage} height={40} width={40} />
     </div>
 
   )
