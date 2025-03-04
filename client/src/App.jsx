2c9ac8452fbd2c5e189d8b174d6284c4ae7f0883
uilacceb <qu6qu6g@gmail.com>
2025-03-03 22:36:06 -0500

    added detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PokemonCard from "./components/Pokemon-Cards/PokemonCard";
 import Layout from "./components/Layout"
+import PokemonDetail from "./components/Pokemon-Details/PokemonDetail";
 
 
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout><PokemonCard /></Layout>} />
+          <Route path="/details" element={<Layout><PokemonDetail /></Layout>} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,19 +5,17 @@ import styles from "./Layout.module.css"
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div className={styles.app}>
-        <div className={styles.header}>
-          <Header />
-        </div>
-        <div className={styles.content}>
-        {children}
-        </div>
-        <div className={styles.footer}>
-          <Footer />
-        </div>
+    <div className={styles.app}>
+      <div className={styles.header}>
+        <Header />
       </div>
-    </>
+      <div className={styles.content}>
+        {children}
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
