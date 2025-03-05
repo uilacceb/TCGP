e4bae085212866a8e93a287b8188dce102ee6632
uilacceb <qu6qu6g@gmail.com>
2025-03-04 19:42:54 -0500

    added login icon in the header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import styles from "./Header.module.css"
 import mainIcon from "../../assets/header-icon.png"
 import cartImage from "../../assets/carts.png"
+import loginIcon from "../../assets/Pokemon_25th_Anniversary_Logo.png"
 
 const Header = () => {
   return (
     <div className={styles.container}>
       <img src={mainIcon} height={80} />
-      <img className={styles.cartImage} src={cartImage} height={40} width={40} />
+      <div className={styles.loginGroup}>
+        <button className={styles.loginButton}> <img className={styles.loginIcon} src={loginIcon} height={50} width={70} />Login</button>
+      </div>
+      <img className={styles.cartImage} src={cartImage} height={50} width={50} />
     </div>
 
   )
