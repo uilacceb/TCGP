0088cef991fe918b5c3775988da27cad2bcb68f7
uilacceb <qu6qu6g@gmail.com>
2025-03-06 16:36:10 -0500

    removed unused file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import cors from "cors"
 import userRouter from "./routes/user.route.js";
 import { ConnectDB } from "./db.js";
 import dotenv from "dotenv"
-import productRouter from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -14,7 +13,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/auth", userRouter)
-app.use("/product", productRouter)
 
 ConnectDB();
 
--- server/routes/product.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { getAllProduct } from "../controllers/product.controller.js";
-
-const productRouter = Router();
-
-productRouter.get("/", getAllProduct)
-
-export default productRouter;
